07a4d3ad964623dfac036de12dab0a525b00927c
Dino Stupar <dinostupar78@gmail.com>
2024-10-19 22:01:13 +0200

    main

--- Stupar-1/src/main/java/hr/java/production/main/Main.java
+++ Stupar-1/src/main/java/hr/java/production/main/Main.java
@@ -1,17 +1,47 @@
 package hr.java.production.main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import hr.java.restaurant.model.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+    private static final Integer numberOfCategories = 3;
+    private static final Integer numberOfIngredients = 5;
+    private static final Integer numberOfMeals = 3;
+    private static final Integer numberOfChefs = 3;
+    private static final Integer numberOfWaiters = 3;
+    private static final Integer numberOfDeliverers = 3;
+    private static final Integer numberOfRestaurants = 3;
+    private static final Integer restaurantAddress = 3;
+    private static final Integer numberOfOrders = 3;
+
+    public static void main(String[] args) {
+        Category[] categories = new Category[numberOfCategories];
+        Ingredient[] ingredients = new Ingredient[numberOfIngredients];
+        Meal[] meals = new Meal[numberOfMeals];
+        Chef[] chefs = new Chef[numberOfChefs];
+        Waiter[] waiters = new Waiter[numberOfWaiters];
+        Deliverer[] deliverers = new Deliverer[numberOfDeliverers];
+        Restaurant[] restaurants = new Restaurant[numberOfRestaurants];
+        Address[] addresses = new Address[restaurantAddress];
+        Order[] orderers = new Order[numberOfOrders];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for(int i = 0; i < categories.length; i++){
+            System.out.println("Unesite podatke za " + (i + 1) + " kategoriju");
+            Category kategorija = categoryInput(scanner);
+            categories[i] = kategorija;
         }
+
+    }
+
+    public static Category categoryInput(Scanner scanner){
+        System.out.println("Unesite ime kategorije: ");
+        String imeKategorije = scanner.nextLine();
+        System.out.println("Unesite opis kategorije: ");
+        String opisKategorije = scanner.nextLine();
+        //System.out.println("Podaci:\n " + imeKategorije + (" ") + opisKategorije);
+        return new Category(imeKategorije, opisKategorije);
     }
 }

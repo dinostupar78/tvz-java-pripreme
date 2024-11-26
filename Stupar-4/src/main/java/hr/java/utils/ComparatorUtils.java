9d37227fa6fedf2f999e16be2ae0d5ca62cbf2df
Dino Stupar <dinostupar78@gmail.com>
2024-11-26 12:03:08 +0100

    changes

--- Stupar-4/src/main/java/hr/java/utils/ComparatorUtils.java
+++ Stupar-4/src/main/java/hr/java/utils/ComparatorUtils.java
@@ -73,7 +73,8 @@ public class ComparatorUtils {
 
         System.out.println("\nJela sortirana po broju restorana:");
         for (Meal meal : mealList) {
-            int restaurantCount = mealRestaurantMap.getOrDefault(meal, List.of()).size();
+            //int restaurantCount = mealRestaurantMap.getOrDefault(meal, List.of()).size();
+            int restaurantCount = mealRestaurantMap.containsKey(meal) ? mealRestaurantMap.get(meal).size() : 0;;
             System.out.println(meal.getName() + " - Broj restorana: " + restaurantCount);
         }
     }
